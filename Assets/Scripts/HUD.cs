c3f3cd5d0f84e987013c12c2374bacac9982a716
Ajeeshi <daniel_best876@hotmail.com>
2016-02-04 15:18:29 +0100

    projekt

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Sprite[] EnergySprites;
+    public Image EnergyUI;
+    public Player player;
+    
+
+    void start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponents<Player>();
+
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerUI : MonoBehaviour
+{
+
+
+    // Stats
+    public int currentEnergy; 
+    public int maxEnergy = 100;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentEnergy = maxEnergy;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentEnergy > maxEnergy)
+        {
+            currentEnergy = maxEnergy;
+        }
+        if (currentEnergy < 0)
+        {
+            Death();
+        }
+
+    }
+
+    void Death() // Detta händer när man dör, restart
+    {
+        // restart after Death
+        SceneManager.LoadScene(1);
+    }
+
+}
+
