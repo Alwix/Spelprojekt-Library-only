034727343bd27fb2a0a4445815e9db1327ae8bc2
Alexander <alexander.bertilsson98@gmail.com>
2016-02-04 07:56:58 +0100

    Assetstest

---
+++ Assets/Scripts/PausScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PausScript : MonoBehaviour
+{
+
+    GameObject PauseMenu;
+    bool paused;
+    bool muted;
+    [SerializeField]
+    Text mutetext;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        paused = false;
+        PauseMenu = GameObject.Find("PauseMenu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = !paused;
+        }
+
+        if (paused)
+        {
+            PauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else if (!paused)
+        {
+            PauseMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+
+    }
+
+    public void Resume()
+    {
+        paused = false;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    /*  public void Save()
+      {
+          PlayerPrefs.SetInt("currentscenesave", SceneManager.LoadScene());
+      }
+    */
+    public void Load()
+    {
+        SceneManager.LoadScene(PlayerPrefs.GetInt("currentscenesave"));
+    }
+
+
+}
